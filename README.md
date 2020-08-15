deb6d810182ec390be52d35de620941f5910ee2f
Evan Ristow <evan.ristow1@gmail.com>
2020-08-15 16:00:44 -0400

    uibuild

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # full_stack_open_part3
+### Heroku URL: https://tranquil-reef-70745.herokuapp.com
---
+++ build/precache-manifest.831931957543c3ae7f67a2d38ee447c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ebd34a283f65ef512e0595a4345e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a30b3b43ae489a2e9e5d",
+    "url": "/static/css/main.bedcb9e2.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a30b3b43ae489a2e9e5d",
+    "url": "/static/js/main.bec3b056.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
